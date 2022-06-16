c68a0ac61ec61ea3d78140504e8c8ea6fd6d9189
Harsh Mangalam <hmangalam@oplglobal.com>
2022-06-16 15:33:36 +0530

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { HopeProvider } from "@hope-ui/solid";
-import type { Component } from "solid-js";
+import { Route, Routes } from "solid-app-router";
+import { Component, lazy, Suspense } from "solid-js";
 
+const HomeRoute = lazy(() => import("./routes/HomeRoute"));
 const App: Component = () => {
-  return <HopeProvider config={{ initialColorMode: "dark" }}></HopeProvider>;
+  return (
+    <HopeProvider config={{ initialColorMode: "dark" }}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/" element={<HomeRoute />}></Route>
+        </Routes>
+      </Suspense>
+    </HopeProvider>
+  );
 };
 
 export default App;
---
+++ src/routes/HomeRoute.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@hope-ui/solid";
+import type { Component } from "solid-js";
+
+const HomeRoute:Component = () => {
+    return (
+        <Box>
+            Home
+        </Box>
+    )
+}
+
+
+export default HomeRoute
