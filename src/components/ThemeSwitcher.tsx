40efdac486e4a1c6a812b1374adef324a6383721
Harsh Mangalam <hmangalam@oplglobal.com>
2022-06-19 15:10:10 +0530

    added theme switcher logic

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,12 +1,14 @@
-import { IconButton } from "@hope-ui/solid";
+import { IconButton, useColorMode } from "@hope-ui/solid";
 import { Component } from "solid-js";
-import { BsSun } from "solid-icons/bs";
+import { BsMoon, BsSun } from "solid-icons/bs";
 
 const ThemeSwitcher: Component = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <IconButton
+      onClick={toggleColorMode}
       aria-label={"Switch to light mode"}
-      icon={<BsSun size={24} />}
+      icon={colorMode() === "dark" ? <BsSun size={24} /> : <BsMoon size={24} />}
       colorScheme="neutral"
       variant={"ghost"}
     />
